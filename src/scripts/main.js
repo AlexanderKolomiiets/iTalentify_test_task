10e7b64d867161ebf4a93ef5b1f0804b07fb3ed8
Sasha Kolomiiets <kinbean444@gmail.com>
2022-11-14 03:58:09 +0200

    added adaptive, footer and functionality of copy button

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const button = document.querySelector('.witcher__code__button');
+const code = document.querySelector('.witcher__code__text');
+
+button.addEventListener('click', () => {
+  navigator.clipboard.writeText(code.textContent)
+    .then(() => {
+      button.textContent = 'copied';
+    })
+    .catch(error => {
+      throw new Error(error);
+    });
+});
